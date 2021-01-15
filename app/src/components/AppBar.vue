6826c591a554b772911a2522422f6a74c916c188
pinxau1000 <31994143+pinxau1000@users.noreply.github.com>
2021-01-15 20:13:17 +0000

    Started digging

---
+++ app/src/components/AppBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-app-bar
+        app
+        absolute
+        color="blue lighten-1"
+        dark
+        dense
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "AppBar"
+}
+</script>
+
+<style scoped>
+
+</style>
