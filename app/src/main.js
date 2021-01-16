7fe49570df2e51aa92211ec97e68d7698e01c581
pinxau1000 <31994143+pinxau1000@users.noreply.github.com>
2021-01-16 23:32:03 +0000

    Added Firebase Database and Hosting

--- app/src/main.js
+++ app/src/main.js
@@ -1,10 +1,20 @@
 // Importing Vue
-import Vue from 'vue'
+import Vue from "vue"
 // Importing Main App Component
-import App from './App.vue'
+import App from "./App.vue"
 // Import Vuetify
-import vuetify from './plugins/vuetify';
+import vuetify from "./plugins/vuetify";
 
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from "firebase/app";
+// Add the Firebase services that you want to use
+import "firebase/auth";
+
+// Import .env
+require("dotenv").config({ path: "../.env" })
+
+// TODO: Production Tip is False
 Vue.config.productionTip = false
 
 // Create a Vue instance, adds vuetify and render App component in element
@@ -13,3 +23,12 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+// TODO: Replace the following with your app's Firebase project configuration
+// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+var firebaseConfig = {
+  host: process.env.DB_CONFIG
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
