229bbe4c3ff9e7975f72ffecbaee34d5fd2e938d
pinxau1000 <31994143+pinxau1000@users.noreply.github.com>
2021-01-17 15:02:32 +0000

    Removed firebase changes.

--- app/src/main.js
+++ app/src/main.js
@@ -5,12 +5,6 @@ import App from "./App.vue"
 // Import Vuetify
 import vuetify from "./plugins/vuetify";
 
-// Firebase App (the core Firebase SDK) is always required and
-// must be listed before other Firebase SDKs
-import * as firebase from "firebase/app";
-// Add the Firebase services that you want to use
-import "firebase/auth";
-
 // Import .env
 require("dotenv").config({ path: "../.env" })
 
@@ -23,12 +17,3 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
-
-// TODO: Replace the following with your app's Firebase project configuration
-// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
-var firebaseConfig = {
-  host: process.env.DB_CONFIG
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
