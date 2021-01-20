079e4089659ffdf2ed7d93ca3fa3aa3fb671c8c9
pinxau1000 <31994143+pinxau1000@users.noreply.github.com>
2021-01-20 15:20:40 +0000

    Starting to add firebase RTDB

--- app/src/main.js
+++ app/src/main.js
@@ -17,3 +17,20 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.DB_APIKEY,
+  authDomain: process.env.DB_AUTHDOMAIN,
+  databaseURL: process.env.DB_DATABASEURL,
+  projectId: process.env.DB_PROJECTID,
+  storageBucket: process.env.DB_STORAGEBUCKET,
+  messagingSenderId: process.env.DB_MESSAGINGSENDERID,
+  appId: process.env.DB_APPID,
+  measurementId: process.env.DB_MEASUREMENTID
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
