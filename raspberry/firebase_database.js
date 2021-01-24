29fae047778590d91ca1fa183c104a88b365d10e
rjfsilva <ricardo_silva_nm@hotmail.com>
2021-01-24 15:12:23 +0000

    missing periodicity

--- raspberry/firebase_database.js
+++ raspberry/firebase_database.js
@@ -1,4 +1,5 @@
 var firebase = require("firebase/app");
+const { v4: uuidv4 } = require('uuid');
 
 // Add the Firebase products that you want to use
 require("firebase/functions"); // ?????????????????
@@ -24,41 +25,167 @@ var database = firebase.database();
 
 module.exports = function () {
     //adds new zone
-    this.addZone = function(zoneId) {
-        firebase.database().ref(zoneId + '/').set({
-            current : 0,
-            enabled : 'true',
-            items : {
-                in : 5,
-                out : 2,
-                desinfect : 4
-            },
-            name : 'Leiria Shopping',
-            periodicityLeds : 10,
-            periodicityDoors : 20,
-            max_ppl : 50,
+    this.createZone = function(zoneName, current=0, max=10) {
+        const uuid = uuidv4();
+        firebase.database().ref(zoneName).set({
+            id: uuid,
+            name: zoneName,
+            current: current,
+            max: max,
+            enabled: true,
+            items: []
       });
     }
 
+
+    /**
+     * Deletes a zone.
+     * @param zoneName The zone name.
+     * @param successCallback The success callback.
+     * @param failureCallback The failure callback.
+     */
+    this.deleteZone = function(zoneName, successCallback, failureCallback){
+        // successCallback is optional
+        successCallback =  (typeof(successCallback) !== "function") ? function(value){return value} : successCallback;
+        // failureCallback is optional
+        failureCallback =  (typeof(failureCallback) !== "function") ? function(error){return error} : failureCallback;
+
+        database.ref(zoneName).remove().then(successCallback).catch(error => failureCallback(error));
+    }
+
+
+    //creates new sensor
+    this.createSensor = function(zoneName, sensorName, periodicity = 5, successCallback, failureCallback){
+        // successCallback is optional
+        successCallback =  (typeof(successCallback) !== "function") ? function(value){return value} : successCallback;
+        // failureCallback is optional
+        failureCallback =  (typeof(failureCallback) !== "function") ? function(error){return error} : failureCallback;
+
+        const uuid = uuidv4();
+        firebase.database().ref(zoneName).child("items").child(sensorName).set({
+            id: uuid,
+            name: sensorName,
+            periodicity: periodicity,
+            values: []
+        }).then(successCallback(uuid)).catch(error => failureCallback(error));
+    }
+
     //get number of ppl inside
-    this.readZone = function (zoneId, callback) {
-        firebase.database().ref(zoneId).once('value', callback);
+    this.readZone = function (zoneName, oncomplete) {
+        firebase.database().ref(zoneName).once('value', oncomplete);
     }
 
     //update zone info
-    this.updateZone = function (zoneId, fieldId, valor) {
-        firebase.database().ref(zoneId + '/').update({
+    this.updateZone = function (zoneName, fieldId, valor) {
+        firebase.database().ref(zoneName).update({
             [fieldId] : valor
         });
     }
+
+    this.increment = function (zoneName, valor) {
+        firebase.database().ref(zoneName).update({
+            current : valor
+        });
+    }
+
+        /**
+     * Adds a sensor value acquired at a given timestamp to the array.
+     * @param zoneName The zone name.
+     * @param sensorName The sensor name.
+     * @param value The new acquired value.
+     * @param timestamp The timestamp when the entry has registered.
+     * @param successCallback A function to handle the complete request with the
+     * reference key of the created entry.
+     * @param failureCallback A function that is called when push fails with the
+     * error as parameter.
+     */
+    newSensorValue = function(zoneName, sensorName, value, timestamp, successCallback, failureCallback){
+        // successCallback is optional
+        successCallback =  (typeof(successCallback) !== "function") ? function(value){return value} : successCallback;
+        // failureCallback is optional
+        failureCallback =  (typeof(failureCallback) !== "function") ? function(error){return error} : failureCallback;
+        database.ref(zoneName).child("items").child(sensorName).child("values").push({
+            value: value,
+            timestamp: timestamp
+        }).then(ret => successCallback(ret.key)).catch(error => failureCallback(error));
+    }
+
+    /**
+     * Adds a newly acquired sensor value to the array.
+     * @param zoneName The zone name.
+     * @param sensorName The sensor name.
+     * @param value The new acquired value.
+     * @param successCallback A function to handle the complete request with the
+     * reference key of the created entry.
+     * @param failureCallback A function that is called when push fails with the
+     * error as parameter.
+     */
+    this.newSensorValueNow = function(zoneName, sensorName, value, successCallback, failureCallback){
+      newSensorValue(zoneName, sensorName, value, Date.now(), successCallback, failureCallback)
+    }
+
+
+    /**
+     * Deletes a sensor.
+     * @param zoneName The zone name.
+     * @param sensorName The sensor name.
+     * @param successCallback The success callback.
+     * @param failureCallback The failure callback.
+     */
+    this.deleteSensor = function(zoneName, sensorName, successCallback, failureCallback){
+        // successCallback is optional
+        successCallback =  (typeof(successCallback) !== "function") ? function(value){return value} : successCallback;
+        // failureCallback is optional
+        failureCallback =  (typeof(failureCallback) !== "function") ? function(error){return error} : failureCallback;
+
+        database.ref(zoneName).child("items").child(sensorName).remove().then(successCallback).catch(error => failureCallback(error));
+    }
+
+
+    /**
+     * Updated the periodicity of a sensor.
+     * @param zoneName The zone name.
+     * @param sensorName The sensor name.
+     * @param periodicity The new periodicity value.
+     * @param successCallback The success callback.
+     * @param failureCallback The failure callback.
+     */
+    this.updatePeriodicitySensor = function(zoneName, sensorName, periodicity, successCallback, failureCallback){
+        // successCallback is optional
+        successCallback =  (typeof(successCallback) !== "function") ? function(value){return value} : successCallback;
+        // failureCallback is optional
+        failureCallback =  (typeof(failureCallback) !== "function") ? function(error){return error} : failureCallback;
+
+        database.ref(zoneName).child("items").child(sensorName).update({
+            "periodicity": periodicity
+        }).then(successCallback).catch(error => failureCallback(error));
+    }
+
+    /**
+     * Runs callback whenever periodicity changed.
+     * @param zoneName The zone name.
+     * @param sensorName The sensor name.
+     * @param successCallback The success callback.
+     * @param failureCallback The failure callback.
+     */
+    this.listeningPeriodicity = function(zoneName, sensorName, successCallback, failureCallback) {
+        // failureCallback is optional
+        failureCallback = (typeof (failureCallback) !== "function") ? function (error) {
+            return error
+        } : failureCallback;
+
+        database.ref(zoneName).child("items").child(sensorName).child("periodicity")
+            .on("value", dataSnapshot => successCallback(dataSnapshot.toJSON()), failureCallback);
+    }
+
 }
 
 
 
 /*
     //get number of ppl inside
-    this.updateZone = function (zoneId, child) {
-        firebase.database().ref(zoneId + '/' + child).once('value', (snapshot) => {
+    this.updateZone = function (zoneName, child) {
+        firebase.database().ref(zoneName + '/' + child).once('value', (snapshot) => {
             const data = snapshot.val();
             console.log(data);
             //return data;
--- raspberry/server.js
+++ raspberry/server.js
@@ -7,8 +7,9 @@ const Gpio = require('onoff').Gpio; //require onoff to control GPIO
 const port = 8080;
 const LEDred = new Gpio(4, 'out');  //declare GPIO4 an output
 const LEDgreen = new Gpio(3, 'out'); //declare GPIO3 an output
+const pushButton = new Gpio(17, 'in', 'rising', {debounceTimeout: 10});
 require('./firebase_database.js')();
-var time = 10;
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -21,56 +22,106 @@ http.listen(port, function (error) {
     console.log('Something went wrong', error);
   }else {
     console.log('Server is listening on port ' + port);
+    console.log('');
   }
 });
 
 
 var callback = function(snapshot) {
+  var time = 10;
+  var mstime = time*1000;
   const data = snapshot.toJSON();
+  zoneName = data.name;
   //console.log(data);
-  if (data.current >= data.max_ppl)  {
+  if (data.current >= data.max)  {
     LEDred.writeSync(1);
     LEDgreen.writeSync(0);
   } else {
     LEDred.writeSync(0);
     LEDgreen.writeSync(1);
   }
-  time = data.periodicityLeds;
-  setTimeout(readZone, time, 'Leiria-Shopping-test', callback);
-  console.log('Time: ' + time)
-  console.log('Lotação máxima: ' + data.max_ppl);
+  let [hour, minute, second] = new Date().toLocaleTimeString('en-US').split(/:| /);
+  setTimeout(readZone, mstime, zoneName, callback);
+  console.log(zoneName);
+  console.log('Periodicity: ' + time + 's')
+  console.log('Lotação máxima: ' + data.max);
   console.log('Lotação atual: ' + data.current);
+  console.log(hour + ':' + minute + ':' + second);
+  console.log('');
+
 }
 
+
+pushButton.watch((err, value) => {
+  if (err) {
+    throw err;
+  }
+  var count;
+  readZone('Leiria-Shopping/current', function(snapshot){
+    count = snapshot.toJSON();
+    count++;
+    console.log('count = ' + count);
+    updateZone('Leiria-Shopping', 'current', count);
+  });
+});
+
+
+function startWebServer() {
+  zonesNames = ['Leiria-Shopping', 'IPLeiria'];
+  sensorsNames = ['Entrada', 'Saida', 'Desinfetante']
+  var z;
+  var current = 0;
+  var max = 5;
+  for (z in zonesNames) {
+    createZone(zonesNames[z], current, max);
+    for (s in sensorsNames) {
+      createSensor(zonesNames[z], sensorsNames[s]);
+      console.log('New ' + sensorsNames[s] + ' sensor!');
+    }
+    console.log('New zone with name: ' + zonesNames[z]);
+    setTimeout(readZone, 5000, zonesNames[z], callback);
+  }
+}
 
 // WebSocket Connection
 io.on('connection', function (socket) {
   var buttonState = 0; //variable to store button state
   var buttonread = 0;
 
-  socket.on('state', function (data) { //get button state from client
-    buttonState = data;
-    if (buttonState != LEDred.readSync()) { //Change LED state if button state is changed
-      LEDred.writeSync(buttonState); //turn LED on or off
-      //addZone('Leiria-Shopping-test', 'Entrada-Norte',  buttonState);
-    }
+  socket.on('inout', function (direction, zoneName) { //get button state from client
+    //console.log(zoneName);
+    readZone(zoneName + '/current', function(snapshot){
+      console.log(snapshot.toJSON());
+      count = snapshot.toJSON();
+      count += direction;
+      updateZone(zoneName, 'current', count);
+    });
   });
 
-  socket.on('read', function() {
-    readZone('Leiria-Shopping-test', callback);
+  socket.on('newsensoreg', function (zoneName, sensor, value) {
+      newSensorValueNow(zoneName, sensor, value);
   });
 
-  socket.on('update', function (zoneId, fieldId, value) {
-    updateZone(zoneId, fieldId,  value);
+  socket.on('read', function(zoneName) {
+    readZone(zoneName, callback);
+    updatePeriodicitySensor(zonename, 'Entrada');
+  });
+
+  socket.on('update', function (zoneName, fieldId, value) {
+    updateZone(zoneName, fieldId,  value);
   })
 
-  socket.on('write', function (zoneId) {
-    addZone(zoneId);
+  socket.on('create', function (zoneName) {
+    createZone(zoneName);
   })
+
+  socket.on('deletezone', function(zoneName) {
+    deleteZone(zoneName);
+  });
 
 });
 
-readZone('Leiria-Shopping-test', callback);
+startWebServer();
 
 
 //on ctrl+c
@@ -79,6 +130,6 @@ process.on('SIGINT', function () {
   LEDred.unexport(); // Unexport LED GPIO to free resources
   LEDgreen.writeSync(0); // Turn LED off
   LEDgreen.unexport(); // Unexport LED GPIO to free resources
-  //pushButton.unexport(); // Unexport Button GPIO to free resources
+  pushButton.unexport(); // Unexport Button GPIO to free resources
   process.exit(); //exit completely
 });
